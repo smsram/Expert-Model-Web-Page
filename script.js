7a24ff5a09c487f8daaec33301ab4e087b3477da
S Meher Siva Ram <smsram001@gmail.com>
2025-02-07 07:40:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,6 +31,7 @@ links.forEach(link => {
 window.addEventListener('scroll', setActiveLink);
 window.addEventListener('load', setActiveLink);
 
+
 // Nav Bar For Mobile
 document.querySelector('.menu-icon').addEventListener('click', function() {
     document.querySelector('.nav-bar').classList.toggle('active');
@@ -79,14 +80,15 @@ window.addEventListener('load', setActiveLink1);
 
 // Scroll Reveal
 ScrollReveal({
-    distance:'80px',
-    duration:2000,
-    delay:200,
-})
+    distance: '20px',
+    duration: 2000,
+    delay: 200,
+    reset: true // If you dont want anmations for every scrolling time then make it false
+});
 ScrollReveal().reveal('.header, .projects-txt, .footer-container2',{origin:'top'});
 ScrollReveal().reveal('.home-container button, .about-container1 div, .home-container, .about-img, .projects-box1, .projects-box2, .projects-box3, .contact-txt, .contact-form #submit, .footer-container1',{origin:'bottom'});
 ScrollReveal().reveal('.home-container h1, .about-container1 h1, .about-container1 h2, .about-txt h1,  .services-box div:nth-child(1), .services-box div:nth-child(3), .success-stories-txt, .become-expert-container h1, .become-expert-container div',{origin:'left'});
-ScrollReveal().reveal('.home-container p, .about-container1 p, .about-txt p:nth-child(2), .services-txt,  .services-box div:nth-child(2), .success-stories-box, .become-expert-container p',{origin:'right'});
+ScrollReveal().reveal('.home-container p, .about-container1 p, .about-txt p:nth-child(2), .services-txt,  .services-box div:nth-child(2), .success-stories-box, .become-expert-container p, .menu-icon',{origin:'right'});
 ScrollReveal().reveal('.about-txt p:nth-child(3), .contact-form #name',{origin:'right', delay: '0400'});
 ScrollReveal().reveal('.about-txt p:nth-child(4), .contact-form #email',{origin:'right', delay: '0800'});
 ScrollReveal().reveal('.about-txt p:nth-child(5), .contact-form #message',{origin:'right', delay: '1200'});
